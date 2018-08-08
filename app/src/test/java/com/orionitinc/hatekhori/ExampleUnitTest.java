2dd3f535fc965e5ca061312b612f174578b60c8f
Ezzaz <37590867+Ezzaz@users.noreply.github.com>
2018-08-08 14:03:51 +0600

    final commit

--- app/src/test/java/com/orionitinc/hatekhori/ExampleUnitTest.java
+++ app/src/test/java/com/orionitinc/hatekhori/ExampleUnitTest.java
@@ -1,12 +1,10 @@
-package com.kvikesh800gmail.relativlayoutjava;
+package com.orionitinc.hatekhori;;
 
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
- */
+
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
